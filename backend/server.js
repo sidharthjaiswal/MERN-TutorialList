d9d6e3d76ec931bcee1dd8e21af2fa6c6574b3c0
Sidharth <sidhankit@gmail.com>
2022-08-06 19:04:10 +0200

    defining Mongoose Model

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const db = require("./models/index.js");
 var corsOptions = {
     origin: "http://localhost:8081"
 };
@@ -19,3 +20,15 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Connected to the database!");
+    })
+    .catch(err => {
+        console.log("Cannot connect to the database!", err);
+        process.exit();
+    });
